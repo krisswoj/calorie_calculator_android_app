3c764eb7e50fc2daf1232d889b457b27ece144a4
Krzysztof Wojdak <krisswoj@gmail.com>
2019-01-11 12:59:09 +0100

    added new view of main page

--- app/src/main/java/com/example/kuba/myapp/MainActivity.java
+++ app/src/main/java/com/example/kuba/myapp/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.kuba.myapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import domain.FetchData;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button click;
+    public static TextView data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        click = findViewById(R.id.btnDisplay);
+        data = findViewById(R.id.fetcheddata);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FetchData process = new FetchData();
+                process.execute();
+            }
+        });
+
     }
 }
