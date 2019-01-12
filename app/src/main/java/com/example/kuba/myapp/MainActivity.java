690fc55b0687c450247fee8b4a2f4dd99664529d
Krzysztof Wojdak <krisswoj@gmail.com>
2019-01-12 19:05:39 +0100

    first version of json download request and reader

--- app/src/main/java/com/example/kuba/myapp/MainActivity.java
+++ app/src/main/java/com/example/kuba/myapp/MainActivity.java
@@ -1,17 +1,29 @@
 package com.example.kuba.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import java.util.concurrent.ExecutionException;
 
 import domain.FetchData;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button click;
+    private Button click;
+    //    private RadioGroup radioGroup;
     public static TextView data;
+
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private Button btnDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +31,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         click = findViewById(R.id.btnDisplay);
-        data = findViewById(R.id.fetcheddata);
 
         click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FetchData process = new FetchData();
-                process.execute();
+                sendMessage(view);
             }
         });
 
+
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Result.class);
+        Bundle extras = new Bundle();
+        FetchData result = new FetchData();
+
+        EditText withoutIngredients = findViewById(R.id.without_ingredients);
+        EditText caloriesAmount = findViewById(R.id.calories_amount);
+        RadioGroup radioGroup = findViewById(R.id.radioKindOfMeal);
+
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+        radioButton = findViewById(selectedId);
+
+        String jsonRespons = null;
+
+        try {
+            jsonRespons = result.execute().get();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        extras.putString("rs1", withoutIngredients.getText().toString());
+        extras.putString("rs2", caloriesAmount.getText().toString());
+        extras.putString("rs3", String.valueOf(radioButton.getText().toString()));
+        extras.putString("rs4", jsonRespons);
+        intent.putExtras(extras);
+
+        startActivity(intent);
     }
 }
--- app/src/main/java/domain/FetchData.java
+++ app/src/main/java/domain/FetchData.java
@@ -2,71 +2,123 @@ package domain;
 
 import android.os.AsyncTask;
 
-import com.example.kuba.myapp.MainActivity;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 /**
  * Created by Abhishek Panwar on 7/14/2017.
  */
 
-public class FetchData extends AsyncTask<Void, Void, Void> {
+public class FetchData extends AsyncTask<Void, Void, String> {
     private String data = "";
     private String dataParsed = "";
     private String singleParsed = "";
 
     @Override
-    protected Void doInBackground(Void... voids) {
+    protected String doInBackground(Void... voids) {
         try {
+
+
+            CloseableHttpClient client = HttpClients.createDefault();
+
+            URIBuilder builder = new URIBuilder("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate");
+            builder
+                    .setParameter("timeFrame", "day")
+//                    .setParameters(nameValuePairList(allegroSellersId))
+                    .setParameter("targetCalories", "2500")
+                    .setParameter("diet", "paleo");
+
+            HttpGet httpGet = new HttpGet(builder.build());
+
+            httpGet.setHeader("X-RapidAPI-Key", "58350e36fbmsh0d1027c7c4adcd2p1dbdd9jsn8aa5925947d2");
+            CloseableHttpResponse response = client.execute(httpGet);
+            String res = httpResponseConverter(response);
+//            String res = response.toString();
+            client.close();
+
+            System.out.println("test json reposne: " + res);
+
+
+
+//            HttpResponse<JsonNode> response = Unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?timeFrame=day&targetCalories=2000&diet=vegetarian&exclude=shellfish%2C+olives")
+//                    .header("X-RapidAPI-Key", "58350e36fbmsh0d1027c7c4adcd2p1dbdd9jsn8aa5925947d2").asJson();
+//
+//            URIBuilder builder = new URIBuilder();
+//            builder.setScheme("https")
+//                    .setHost("spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
+//                    .setPath("/recipes/mealplans/generate")
+//                    .setParameter("timeFrame", "day")
+//                    .setParameter("targetCalories", "2500")
+//                    .setParameter("diet", "paleo")
+//                    .setParameter("exclude", "oils")
+//                    .setParameter("X-RapidAPI-Key", "58350e36fbmsh0d1027c7c4adcd2p1dbdd9jsn8aa5925947d2");
+
+//            builder.build().toString();
+
+
+
+//            URL url = new URL("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate");
+//            URL url = new URL(builder.build().toString());
+
+//            System.out.println("test json reposne: " + response.getBody().toString());
+
+
             URL url = new URL("https://api.myjson.com/bins/j5f6b");
+//            URL url = new URL("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?timeFrame=day&targetCalories=2000&diet=vegetarian&exclude=shellfish%2C+olives");
+
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             InputStream inputStream = httpURLConnection.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
             String line = "";
             while (line != null) {
                 line = bufferedReader.readLine();
-                data = data + line;
+                data += line;
             }
 
-            JSONArray JA = new JSONArray(data);
-            for (int i = 0; i < JA.length(); i++) {
-                JSONObject JO = (JSONObject) JA.get(i);
-                singleParsed = "Name:" + JO.get("name") + "\n" +
-                        "Password:" + JO.get("password") + "\n" +
-                        "Contact:" + JO.get("contact") + "\n" +
-                        "Country:" + JO.get("country") + "\n";
-
-                dataParsed = dataParsed + singleParsed + "\n";
-
-
-            }
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
 
-        return null;
+        return data;
     }
 
     @Override
-    protected void onPostExecute(Void aVoid) {
+    protected void onPostExecute(String aVoid) {
         super.onPostExecute(aVoid);
-
-        MainActivity.data.setText(this.dataParsed);
+//        MainActivity.data.setText(this.dataParsed);
 
     }
+
+    private String httpResponseConverter(CloseableHttpResponse response) throws IOException {
+
+        String res = null;
+        HttpEntity entity = response.getEntity();
+
+        if (entity != null) {
+            InputStream instream = entity.getContent();
+
+
+            byte[] bytes = IOUtils.toByteArray(instream);
+            res = new String(bytes, "UTF-8");
+            instream.close();
+        }
+        return res;
+    }
+
+//    private List<NameValuePair> nameValuePairs (String timeFrame, int targetCalories, String diet, String exclude){
+//        List<NameValuePair> nameValuePairs = new
+//    }
 }
